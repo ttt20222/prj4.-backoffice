5d712a004bb4b05b24a0c66dcc596e438320af0e
yewon kim <yewonkim@yewonui-MacBookPro.local>
2024-06-18 19:13:07 +0900

    updat. 메뉴 생성 조회 API 구현

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
 import express from "express";
 import { SERVER_PORT } from "./constants/env.constant.js";
+import "./utils/prisma/index.js";
+import { apiRouter } from "./routers/index.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use("/api", apiRouter);
+
 app.listen(SERVER_PORT, () => {
-    console.log(SERVER_PORT, '포트로 서버가 열렸어요!');
-  });
+  console.log(SERVER_PORT, "포트로 서버가 열렸어요!");
+});
---
+++ src/controllers/menus.controller.js
@@ -0,0 +1,57 @@
+import { HTTP_STATUS } from "../constants/http-status.constant.js";
+import { MenusService } from "../services/menus.service.js";
+
+export class MenusController {
+  menusService = new MenusService();
+  create = async (req, res, next) => {
+    try {
+      //path parameters에서 restaurantId 추출
+      const restaurantId = parseInt(req.params.restaurantId);
+      //req.body에서 데이터 추출
+      const { menuName, menuPrice, menuType, menuDescription, menuImageUrl } =
+        req.body;
+
+      const menu = await this.menusService.create(
+        restaurantId,
+        menuName,
+        menuPrice,
+        menuType,
+        menuDescription,
+        menuImageUrl
+      );
+
+      return res.status(HTTP_STATUS.CREATED).json({
+        status: HTTP_STATUS.CREATED,
+        message: "메뉴 등록이 완료되었습니다.",
+        menu,
+      });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  readAll = async (req, res, next) => {
+    try {
+      //path parameters에서 restaurantId 추출
+      const { restaurantId } = req.params;
+      const menus = await this.menusService.readAll(restaurantId);
+
+      return res.status(HTTP_STATUS.OK).json({ status: HTTP_STATUS.OK, menus });
+    } catch (error) {
+      next(error);
+    }
+  };
+
+  readById = async (req, res, next) => {
+    try {
+      //path parameters에서 restaurantId, menuId 추출
+      const { restaurantId, menuId } = req.params;
+
+      const menu = await this.menusService.readById(restaurantId, menuId);
+
+      return res.status(HTTP_STATUS.OK).json({ status: HTTP_STATUS.OK, menu });
+    } catch (error) {
+      next(error);
+    }
+  };
+}
