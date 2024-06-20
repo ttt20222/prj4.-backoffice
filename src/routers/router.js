ffc671cc4bbe89e076dcfe5ed6d81a3d03837d47
PARKGEUNGYUN <pcpp0606@naver.com>
2024-06-20 15:49:32 +0900

    주석 및 수정

--- src/routers/router.js
+++ src/routers/router.js
@@ -1,7 +1,6 @@
 import express from 'express';
 import { authRouter } from './auth.router.js';
 import { usersRouter } from './users.router.js';
-import { requireAccessToken } from '../middlewares/require-access-token.middleware.js';
 
 const apiRouter = express.Router();
 
